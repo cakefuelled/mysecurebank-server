5a4688924c7382010f1b28129da631520d5c5d4e
Alejandro U. Alvarez <aluralma@gmail.com>
2016-11-26 16:51:53 +0000

    use mongodb driver

--- config/passport.js
+++ config/passport.js
@@ -11,6 +11,7 @@ const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const OpenIDStrategy = require('passport-openid').Strategy;
 const OAuthStrategy = require('passport-oauth').OAuthStrategy;
 const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var MongoClient = require('mongodb').MongoClient;
 
 const User = require('../models/User');
 
@@ -28,19 +29,17 @@ passport.deserializeUser((id, done) => {
  * Sign in using Email and Password.
  */
 passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-  User.findOne({ email: email.toLowerCase() }, (err, user) => {
-    if (err) { return done(err); }
-    if (!user) {
-      return done(null, false, { msg: `Email ${email} not found.` });
-    }
-    user.comparePassword(password, (err, isMatch) => {
-      if (err) { return done(err); }
-      if (isMatch) {
-        return done(null, user);
-      }
-      return done(null, false, { msg: 'Invalid email or password.' });
+    var url = 'mongodb://localhost:27017/test';
+    MongoClient.connect(url, function(err, db) {
+      var query = {
+        $where: "this.email === " + email
+      };
+
+      db.collection('users').find(query).each(function(err, user) {
+        console.log(user);
+      });
+      db.close();
     });
-  });
 }));
 
 /**
